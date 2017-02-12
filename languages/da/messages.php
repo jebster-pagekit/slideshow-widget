406045cc1a6b6ba847554e19f66e3858e404414a
Jógvan Olsen <jo@jebster.net>
2017-02-12 15:39:15 +0100

    Updated languages

---
+++ languages/da/messages.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'Height' => '',
+  'Time interval' => '',
+  'Header' => '',
+  'Speed' => '',
+  'Animation' => '',
+  'Title cannot be blank.' => '',
+  'Title' => '',
+  'Image' => '',
+);
---
+++ languages/fo/messages.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'Height' => 'Hædd',
+  'Time interval' => '',
+  'Header' => 'Yvirskrift',
+  'Speed' => '',
+  'Animation' => '',
+  'Title cannot be blank.' => 'Heiti kann ikki vera tømt',
+  'Title' => 'Heiti',
+  'Image' => 'Mynd',
+);
